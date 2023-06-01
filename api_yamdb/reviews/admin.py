fe11c52aa556c47bce02ae88558563fa7203e202
Полина Киселева <polly-kiselyova@yandex.ru>
2023-06-01 13:15:26 +0400

    admin.py

---
+++ api_yamdb/reviews/admin.py
@@ -0,0 +1,37 @@
+from django.contrib import admin
+
+from .models import Category, Title, Genre
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'slug',
+    )
+    search_fields = ('name',)
+    list_filter = ('name',)
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'slug'
+    )
+    search_fields = ('name',)
+    list_filter = ('name',)
+    empty_value_display = '-пусто-'
+
+@admin.register(Title)
+class TitleAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'descripsion',
+        'genre',
+        'category',
+    )
+    search_fields = ('name',)
+    list_filter = ('name',)
+    empty_value_display = '-пусто-'
